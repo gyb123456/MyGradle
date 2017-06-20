1b1139c7fcb3afc526fece1d5f172cdda03880bb
yaomin <15221346179@163.com>
2017-06-20 16:29:12 +0800

    modify route and station interface

--- src/main/java/com/base/biz/StationBiz.java
+++ src/main/java/com/base/biz/StationBiz.java
@@ -4,9 +4,21 @@ import java.util.List;
 
 import com.base.po.Station;
 
+/**站点表相关接口
+ * @author yaomin
+ * */
 public interface StationBiz {
-	//根据线路编号和方向查询站点信息
-	List<Station> findByRtNumAndDirec(Integer routeNum,Integer direction);
-	//根据站点名查询站点信息
-	List<Station> findByName(String name);
+	
+	/**根据线路编号和方向查询站点信息
+	 * @param routeNum 线路编号
+	 * @param direction 方向
+	 * @return List<Station>
+	 */
+	public List<Station> findByRtNumAndDirec(Integer routeNum,Integer direction);
+	
+	/**根据站点名查询站点信息
+	 * @param name 名称
+	 * @return List<Station>
+	 */
+	public List<Station> findByName(String name);
 }
--- src/main/java/com/base/dao/RouteDao.java
+++ src/main/java/com/base/dao/RouteDao.java
@@ -5,19 +5,50 @@ import java.util.Map;
 
 import com.base.po.Route;
 
+/**线路表相关接口
+ * @author yaomin
+ * */
 public interface RouteDao {
-	//分页查询
-	List<Route> findByPageAndParams(int page,int num,Integer routeNum);
-	//根据线路名查询线路信息
-	List<Route> findByName(String name);
-	//根据线路名模糊查询线路信息
-	List<Route> findByNameLike(String name);	
-	//根据线路编号查询线路信息
-	Route findByNum(Integer routeNum);
-	//查询所有线路的线路名和线路编号
-	List<Map> findNameAndNum();	
-	//根据线路编号查询首末站信息
-	List<Map> findTermByNum(Integer routeNum);	
-	//根据线路编号查询首末班车信息
-	List<Map> findDepartTimeByNum(Integer routeNum);	
+	/**分页查询
+	 * @param page 当前页数
+	 * @param num 每页的数据量
+	 * @param routeNum 线路编号
+	 * @return List<Route>
+	 */
+	public List<Route> findByPageAndParams(int page,int num,Integer routeNum);
+	
+	/**根据线路名称查询线路信息
+	 * @param name 线路名称
+	 * @return List<Route>
+	 */
+	public List<Route> findByName(String name);
+	
+	/**根据线路名称模糊查询线路名称和线路编号
+	 * @param name 线路名称
+	 * @return List<Map>  [{routeNum:12,name:""},{routeNum:13,name:""}]
+	 */
+	public List<Map> findNameAndNumByNameLike(String name);	
+	
+	/**根据线路编号查询线路信息
+	 * @param routeNum 线路编号
+	 * @return Route
+	 */	
+	public Route findByNum(Integer routeNum);
+	
+	/**查询所有线路的线路名和线路编号
+	 * @return List<Map>  [{routeNum:12,name:""},{routeNum:13,name:""}]
+	 */	
+	public List<Map> getAllNameAndNum();	
+	
+	/**根据线路编号查询首末站信息
+	 * @param routeNum 线路编号
+	 * @return List<Map>  [{upTrmlStation:"",downTrmlStation:""},{upTrmlStation:"",downTrmlStation:""}]
+	 */	
+	public List<Map> findTermByNum(Integer routeNum);	
+	
+	/**根据线路编号查询首末班车信息
+	 * @param routeNum 线路编号
+	 * @return List<Map>  [{upFirstTime:"",upLastTime:"",downFirstTime:"",downLastTime:""}]
+	 */
+	public List<Map> findDepartTimeByNum(Integer routeNum);	
 }
