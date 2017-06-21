1eccb82cee3998499741c42b1125d8d0b18d056f
yaomin <15221346179@163.com>
2017-06-21 13:38:28 +0800

    add LinuxStateTest.java

---
+++ src/test/java/com/servmonit/LinuxStateTest.java
@@ -0,0 +1,25 @@
+package com.servmonit;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.RootConfig;
+import com.jcraft.jsch.JSchException;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=RootConfig.class)
+public class LinuxStateTest {
+
+	@Test
+	public void stateTest() throws IOException, JSchException{
+		String result = LinuxState.runDistanceShell("root", "ITScai@207", "10.1.30.207"); 
+		System.out.println("读取的信息："+result);
+		System.out.println("转换后的信息："+LinuxState.disposeResultMessage(result));
+	}
+}
