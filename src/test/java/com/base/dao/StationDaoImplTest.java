c5c46c9436ac55f6dde2f3f5ecfe9e4764de1ce3
yaomin <15221346179@163.com>
2017-06-22 15:41:55 +0800

    modify station test

--- src/test/java/com/base/dao/StationDaoImplTest.java
+++ src/test/java/com/base/dao/StationDaoImplTest.java
@@ -8,7 +8,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.RootConfig;
-import com.base.dao.impl.RouteDaoImpl;
 import com.base.dao.impl.StationDaoImpl;
 
 @RunWith(SpringJUnit4ClassRunner.class)
